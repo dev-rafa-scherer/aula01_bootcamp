6d4bd04bdb63a9af0445400f23835dc51af3fddb
Rafael Scherer <dev.rafa23@gmail.com>
2025-07-16 22:02:52 -0300

    adicionar o exercicio 2

---
+++ exercicio02.py
@@ -0,0 +1,5 @@
+# Crie um programa onde o usuario digite dois valores e apareca a soma entre eles
+
+a = (int(input('Digite o primeiro valor: ')))
+b = (int(input('Digite o segundo valor: ')))   
+print('A soma entre os dois valores é: ', a + b) 
